2fb3de863c5a6b1fc80f0a33d79411df3f3d0525
Charles Paxson <30535060+chpaxson@users.noreply.github.com>
2024-01-24 13:11:23 -0800

    Update can_driver.py

--- Software/driver/can_driver.py
+++ Software/driver/can_driver.py
@@ -5,15 +5,17 @@ from CANableBus import CANableBus
 bus = CANableBus()
 
 def main():
-    print("a")
+    print("Starting movement test...")
     set_work_mode(1, "SR_vFOC")
     set_work_mode(2, "SR_vFOC")
     time.sleep(0.2)
     set_microsteps(1, 16)
     set_microsteps(2, 16)
     time.sleep(0.2)
-    set_working_current(1, 1000)
-    set_working_current(2, 500)
+    print(bus.read_input(1))
+    print(bus.read_input(2))
+    set_working_current(1, 2000)
+    set_working_current(2, 2000)
     time.sleep(0.2)
     set_holding_current_percent(1, 20)
     set_holding_current_percent(2, 20)
@@ -21,10 +23,19 @@ def main():
     enable_motor(1)
     enable_motor(2)
     time.sleep(0.2)
-    # move_relative(1, "CCW", 1000, 100, 3000)
-    move_relative(2, "CW", 3000, 100, 3000)
-    # move_relative_revs(1, "CW", 3000, 0, 20)
-    move_absolute(1, 3000, 0, 0)
+    speed = 100
+    accel = 128
+    move_time1 = move_relative(1, "CCW", speed, accel, 3200)
+    move_time2 = move_relative(2, "CCW", speed, accel, 3200)
+    time.sleep(max(move_time1, move_time2) + 0.2)
+    move_time1 = move_relative(1, "CW", speed, accel, 3200)
+    move_time2 = move_relative(2, "CW", speed, accel, 3200)
+    time.sleep(max(move_time1, move_time2) + 0.2)
+    # move_absolute(1, 2000, 128, 0)
+    # move_absolute(2, 2000, 128, 0)
+    # time.sleep(10)
+    # disable_motor(1)
+    # disable_motor(2)
     print(bus.read_input(1))
 
 def calculate_crc(arbitration_id, data: list) -> int:
@@ -58,11 +69,13 @@ def set_work_mode(motor, mode):
         return
     bytes = [0x82, mode_byte]
     send_msg(motor, bytes)
+    print("Motor " + str(motor) + " work mode set to " + mode)
 
 def set_working_current(motor, current):
     # Split current (16 bit int) into two bytes and append to bytes list
     bytes = [0x83] + list(current.to_bytes(2, byteorder='big'))
     send_msg(motor, bytes)
+    print("Motor " + str(motor) + " working current set to " + str(current))
 def set_holding_current_percent(motor, percent):
     percent = round(percent/10, 0)
     if percent < 1:
@@ -72,22 +85,25 @@ def set_holding_current_percent(motor, percent):
     percent -= 1
     bytes = [0x9B] + list(int(percent).to_bytes(1, byteorder='big'))
     send_msg(motor, bytes)
+    print("Motor " + str(motor) + " holding current percent set to " + str(percent*10))
 
 def set_microsteps(motor, microsteps):
     # Check that microsteps is a power of 2, up to 256.
-    if microsteps < 1 or microsteps > 256 or (microsteps & (microsteps - 1)) != 0:
-        print("Invalid microsteps")
-        return
-    microsteps -= 1
+    # if microsteps < 1 or microsteps > 256 or (microsteps & (microsteps - 1)) != 0:
+    #     print("Invalid microsteps")
+    #     return
     bytes = [0x84, microsteps]
     send_msg(motor, bytes)
+    print("Motor " + str(motor) + " microsteps set to " + str(microsteps))
 
 def enable_motor(motor):
     bytes = [0x84, 0x01]
     send_msg(motor, bytes)
+    print("Motor " + str(motor) + " enabled")
 def disable_motor(motor):
     bytes = [0x84, 0x00]
     send_msg(motor, bytes)
+    print("Motor " + str(motor) + " disabled")
 
 # Manual page 39
 def move_relative(motor, dir, speed, accel, pulses):
@@ -106,6 +122,8 @@ def move_relative(motor, dir, speed, accel, pulses):
     bytes = [0xFD, byte2, byte3, byte4] + list(pulse_bytes)
     print(bytes)
     send_msg(motor, bytes)
+    move_time = 60 * pulses / (200 * 16 * speed)
+    return move_time
 
 # Manual page 41
 def move_absolute(motor, speed, accel, pulses):
@@ -114,9 +132,9 @@ def move_absolute(motor, speed, accel, pulses):
     bytes = [0xFE, speed_bytes[0], speed_bytes[1], accel] + list(pulse_bytes)
     send_msg(motor, bytes)
 
-def move_relative_revs(motor, dir, speed, accel, revs):
-    pulses = revs * 200
-    move_relative(motor, dir, speed, accel, pulses)
+def estop(motor):
+    bytes = [0xF7]
+    send_msg(motor, bytes)
 
 if __name__ == "__main__":
     main()
