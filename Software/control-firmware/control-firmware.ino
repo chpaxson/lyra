bed0a89631a01b141d90925d4ddbf239dbc35fc9
Charles Paxson <chpaxson@berkeley.edu>
2024-04-22 04:46:40 -0700

    basic demo movement

--- Software/control-firmware/control-firmware.ino
+++ Software/control-firmware/control-firmware.ino
@@ -1,4 +1,5 @@
-#include "AA_MCP2515.h"
+#include "AccelStepper.h"
+#include "MultiStepper.h"
 /*==== Motor Pins ====*/
 #define MOT_EN 13
 
@@ -16,162 +17,87 @@
 #define MOT5_DIR 26
 #define MOT6_DIR 27
 
-/*==== CAN Config ====*/
-#define CAN_CS  53
-#define CAN_INT 21
+const int steps_per_rev = 200 * 16;
+long motor_step_positions[] = {0, 0, 0, 0, 0, 0};
+int gear_ratios[] = {16, 24, 19, 15, 15, 15};
 
-const CANBitrate::Config CAN_BITRATE = CANBitrate::Config_8MHz_500kbps;
+// Set up stepper objects
+AccelStepper stepper1(AccelStepper::DRIVER, MOT1_STP, MOT1_DIR);
+AccelStepper stepper2(AccelStepper::DRIVER, MOT2_STP, MOT2_DIR);
+AccelStepper stepper3(AccelStepper::DRIVER, MOT3_STP, MOT3_DIR);
+AccelStepper stepper4(AccelStepper::DRIVER, MOT4_STP, MOT4_DIR);
+AccelStepper stepper5(AccelStepper::DRIVER, MOT5_STP, MOT5_DIR);
+AccelStepper stepper6(AccelStepper::DRIVER, MOT6_STP, MOT6_DIR);
 
-CANConfig config(CAN_BITRATE, CAN_CS, CAN_INT);
-CANController CAN(config);
+MultiStepper steppers;
 
 void setup() {
     // Begin Serial port
     Serial.begin(38400);
+    // Set up the enable pin
     pinMode(MOT_EN, OUTPUT);
-    // Motor 1
-    pinMode(MOT1_STP, OUTPUT);
-    pinMode(MOT1_DIR, OUTPUT);
+    digitalWrite(MOT_EN, LOW);
 
-    digitalWrite(MOT1_DIR, HIGH);
-    digitalWrite(MOT_EN, HIGH);
-    // Set up Motor 1
-    setWorkMode(1, 1);
-    setWorkCurrent(1, 1000);
-    setHoldCurrent(1, 3);
-    setMicrosteps(1, 16);
-    enableBehavior(1, 1);
+    steppers.addStepper(stepper1);
+    steppers.addStepper(stepper2);
+    steppers.addStepper(stepper3);
+    steppers.addStepper(stepper4);
+    steppers.addStepper(stepper5);
+    steppers.addStepper(stepper6);
+    delay(5000);
+    // Run a demo sequence
+    float accel_factor = 0.00001;
+    simultaneous_move_degrees(5.0, accel_factor, 60, 75, 80, 0, 0, 0);
+    delay(100);
+    simultaneous_move_degrees(3.0, accel_factor, 75, 140, 50, 0, 0, 0);
+    delay(500);
+    simultaneous_move_degrees(1.0, accel_factor, 75, 130, 45, 0, 0, 0);
+    delay(100);
+    simultaneous_move_degrees(1.0, accel_factor, 45, 130, 45, 0, 0, 0);
+    delay(100);
+    simultaneous_move_degrees(1.0, accel_factor, 45, 140, 50, 0, 0, 0);
+    delay(500);
+    simultaneous_move_degrees(7.5, accel_factor, 0, 0, 0, 0, 0, 0);
+    
+    
 }
 
-void loop() {
-    digitalWrite(MOT1_STP, HIGH);
-    delay(10);
-    digitalWrite(MOT1_STP, LOW);
-    delay(10);
+void loop() {}
+
+void simultaneous_move_steps(float move_time, float accel_factor, int m1, int m2, int m3, int m4, int m5, int m6) {
+    long m1_dist = m1 - motor_step_positions[0];
+    long m2_dist = m2 - motor_step_positions[1];
+    long m3_dist = m3 - motor_step_positions[2];
+    long m4_dist = m4 - motor_step_positions[3];
+    long m5_dist = m5 - motor_step_positions[4];
+    long m6_dist = m6 - motor_step_positions[5];
+    stepper1.setMaxSpeed(m1_dist / move_time);
+    stepper2.setMaxSpeed(m2_dist / move_time);
+    stepper3.setMaxSpeed(m3_dist / move_time);
+    stepper4.setMaxSpeed(m4_dist / move_time);
+    stepper5.setMaxSpeed(m5_dist / move_time);
+    stepper6.setMaxSpeed(m6_dist / move_time);
+    stepper1.setAcceleration(accel_factor * m1_dist / move_time);
+    stepper2.setAcceleration(accel_factor * m2_dist / move_time);
+    stepper3.setAcceleration(accel_factor * m3_dist / move_time);
+    stepper4.setAcceleration(accel_factor * m4_dist / move_time);
+    stepper5.setAcceleration(accel_factor * m5_dist / move_time);
+    stepper6.setAcceleration(accel_factor * m6_dist / move_time);
+    motor_step_positions[0] = m1;
+    motor_step_positions[1] = m2;
+    motor_step_positions[2] = m3;
+    motor_step_positions[3] = m4;
+    motor_step_positions[4] = m5;
+    motor_step_positions[5] = m6;
+    steppers.moveTo(motor_step_positions);
+    steppers.runSpeedToPosition();
 }
-
-/*
-    0 - Pulse interface, open loop
-    1 - Pulse interface, closed loop
-    2 - Pulse interface, FOC
-    3 - Serial interface, open loop
-    4 - Serial interface, closed loop
-    5 - Serial interface, FOC
-*/
-void setWorkMode(int motor, int mode) {
-    sendCommand(motor, {0x82, mode});
-    Serial.print("Motor ");
-    Serial.print(motor);
-    Serial.print(" work mode set to ");
-    switch mode {
-        case 0:
-            Serial.println("CR_OPEN");
-            break;
-        case 1:
-            Serial.println("CR_CLOSE");
-            break;
-        case 2:
-            Serial.println("CR_FOC");
-            break;
-        case 3:
-            Serial.println("SR_OPEN");
-            break;
-        case 4:
-            Serial.println("SR_CLOSE");
-            break;
-        case 5:
-            Serial.println("SR_FOC");
-            break;
-    }
-}
-
-void setWorkCurrent(int motor, int current) {
-    sendCommand(motor, {0x83, current >> 8, current & 0xFF});
-    Serial.print("Motor ");
-    Serial.print(motor);
-    Serial.print(" work current set to ");
-    Serial.print(current);
-    Serial.println(" mA");
-}
-
-/*
-    Input as a percentage
-    0 -> 10%, 1 -> 20%, 2 -> 30%, 3 -> 40%, 4 -> 50%, 5 -> 60%, 6 -> 70%, 7 -> 80%, 8 -> 90%
-*/
-void setHoldCurrent(int motor, int currentPercent) {
-    sendCommand(motor, {0x9B, currentPercent / 10 - 1});
-    Serial.print("Motor ");
-    Serial.print(motor);
-    Serial.print(" hold current set to ");
-    Serial.print(currentPercent * 10);
-    Serial.println(" mA");
-}
-
-void setMicrosteps(int motor, int microsteps) {
-    sendCommand(motor, {084A, microsteps});
-    Serial.print("Motor ");
-    Serial.print(motor);
-    Serial.print(" microsteps set to ");
-    Serial.println(microsteps);
-}
-
-/*
-    0 - Active when pin is low
-    1 - Active when pin is high
-    2 - Always active
-*/
-void enableBehavior(int motor, int behavior) {
-    sendCommand(motor, {0x85, behavior});
-    Serial.print("Motor ");
-    Serial.print(motor);
-    Serial.print(" enable behavior set to be ");
-    switch behavior {
-        case 0:
-            Serial.println("active when pin is low");
-            break;
-        case 1:
-            Serial.println("active when pin is high");
-            break;
-        case 2:
-            Serial.println("always active");
-            break;
-    }
-}
-
-/*
-    Direction is the first bit of the second byte
-    Speed is 12 bits long, starting at the 5th bit of the second byte and completing the third byte
-    Acceleration is the fourth byte
-    Pulses is 24 bits long, composing bytes 5, 6, and 7
-*/
-void move_relative(int motor, bool dir, uint16_t speed, uint8_t accel, uint32_t pulses) {
-    byte byte2 = (dir << 8) | (speed >> 8);
-    byte byte3 = speed & 0b000011111111;
-    byte byte4 = accel;
-    byte byte5 = pulses >> 16;
-    byte byte6 = pulses >> 8;
-    byte byte7 = pulses;
-    sendCommand(motor, {0xFD, byte2, byte3, byte4, byte5, byte6, byte7});
-    Serial.print("Motor ");
-    Serial.print(motor);
-    Serial.print(" moving ");
-    if (dir) {
-        Serial.print("clockwise ");
-    } else {
-        Serial.print("counter-clockwise ");
-    }
-    Serial.print(pulses);
-    Serial.println(" pulses");
-}
-
-
-// Send a frame to the motor controller
-void sendCommand(int motorAddress, int[] commandData) {
-    crc = motorAddress;
-    for (int i = 0; i < sizeof(commandData); i++) {
-        crc = crc + commandData[i];
-    }
-    crc = crc % 256;
-    byte data[] = {motorAddress, commandData, crc};
-    CAN.write(motorAddress, data, sizeof(data));
+void simultaneous_move_degrees(float move_time, float accel_factor, float m1, float m2, float m3, float m4, float m5, float m6) {
+    long m1_steps = m1 * steps_per_rev * gear_ratios[0] / 360;
+    long m2_steps = m2 * steps_per_rev * gear_ratios[1] / 360;
+    long m3_steps = m3 * steps_per_rev * gear_ratios[2] / 360;
+    long m4_steps = m4 * steps_per_rev * gear_ratios[3] / 360;
+    long m5_steps = m5 * steps_per_rev * gear_ratios[4] / 360;
+    long m6_steps = m6 * steps_per_rev * gear_ratios[5] / 360;
+    simultaneous_move_steps(move_time, accel_factor, m1_steps, m2_steps, m3_steps, m4_steps, m5_steps, m6_steps);
 }
